1ca07d049333fe944cb635e56d2f41d98a4b013d
viniciusjf10 <viniciusouzajf@gmail.com>
2021-06-01 14:14:57 -0300

    Exercicio Relampago

---
+++ Aula01/src/main/java/Funcionario.java
@@ -0,0 +1,43 @@
+public class Funcionario {
+        private String nome;
+        private float salarioBruto;
+        private float totalAcrescimo;
+        private float totalDesconto;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public float getSalarioBruto() {
+            return salarioBruto;
+        }
+
+        public void setSalarioBruto(float salarioBruto) {
+            this.salarioBruto = salarioBruto;
+        }
+
+        public float getTotalAcrescimo() {
+            return totalAcrescimo;
+        }
+
+        public void setTotalAcrescimo(float totalAcrescimo) {
+            this.totalAcrescimo = totalAcrescimo;
+        }
+
+        public float getTotalDesconto() {
+            return totalDesconto;
+        }
+
+        public void setTotalDesconto(float totalDesconto) {
+            this.totalDesconto = totalDesconto;
+        }
+
+        public float calculaSalarioLiquido (){
+            return salarioBruto+totalAcrescimo-totalDesconto;
+        }
+
+    }
---
+++ Aula01/src/main/java/Principal.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+
+        Scanner scanner = new Scanner( System.in );
+        System.out.println("Digite o nome do funcionario: ");
+        String nome = scanner.next();
+        funcionario.setNome(nome);
+
+        Scanner scanner2 = new Scanner( System.in );
+        System.out.println("Digite o salario bruto do funcionario: ");
+        float salarioB = scanner.nextFloat();
+        funcionario.setSalarioBruto(salarioB);
+
+        Scanner scanner3 = new Scanner( System.in );
+        System.out.println("Digite o total de acrescimos no salário do funcionario: ");
+        float salarioA = scanner.nextFloat();
+        funcionario.setTotalAcrescimo(salarioA);
+
+        Scanner scanner4 = new Scanner( System.in );
+        System.out.println("Digite o total de descontos no salário do funcionario: ");
+        float salarioD = scanner.nextFloat();
+        funcionario.setTotalDesconto(salarioD);
+
+        /*  SET MANUAL DE VALORES
+        funcionario.setNome("Vinicius");
+        funcionario.setSalarioBruto(4500);
+        funcionario.setTotalDesconto(1000);
+        funcionario.setTotalAcrescimo(1500);
+        SET MANUAL DE VALORES */
+
+        System.out.println("O salario final do funcionario e: " + funcionario.calculaSalarioLiquido());
+    }
+}
